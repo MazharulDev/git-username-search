ece8556f2568939cdd5628715bf71704ccd9607d
Md Mazharul islam <miforbd@gmail.com>
2022-04-03 10:10:25 +0600

    test comment

--- src/components/PersonDetails/PersonDetails.js
+++ src/components/PersonDetails/PersonDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+// personal details page 
 const PersonDetails = () => {
     const {id}=useParams();
     const [user,setUser]=useState({});
