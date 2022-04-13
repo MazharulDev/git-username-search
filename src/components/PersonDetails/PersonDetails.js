b6c6285a5b919463f7946181d694a82dc5e413d0
Md Mazharul islam <miforbd@gmail.com>
2022-04-13 11:57:36 +0600

    personal details design added and icon to

--- src/components/PersonDetails/PersonDetails.js
+++ src/components/PersonDetails/PersonDetails.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 // personal details page 
 const PersonDetails = () => {
-    const {id}=useParams();
-    const [user,setUser]=useState({});
+    const { id } = useParams();
+    const [user, setUser] = useState({});
     console.log(user);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`https://api.github.com/users/${id}`)
-        .then(res=>res.json())
-        .then(data=>setUser(data))
-    },[id])
+            .then(res => res.json())
+            .then(data => setUser(data))
+    }, [id])
     return (
         <div className=''>
-            <img className='mx-auto mt-4 rounded-lg' src={user.avatar_url} alt="" />
-            <h2>{user.name}</h2>
-            <p>Bio: {user.bio? user.bio:'Not Found'}</p>
-            <h4>Location: {user.location? user.location:'Not Found'}</h4>
-            <div className='flex justify-center gap-5'>
-                <h3>Followers: {user.followers}</h3>
-                <h3>Following: {user.following}</h3>
+            <div className='w-7/12 mx-auto border-4 rounded-2xl mt-5 bg-indigo-200'>
+                <img width={300} className='mx-auto mt-4 rounded-full border-8 hover:scale-105 border-indigo-500 mb-4' src={user.avatar_url} alt="" />
+                <div className='rounded-br-xl rounded-bl-xl'>
+                    <h2 className='bg-blue-600 inline-block p-2 rounded-md text-white hover:scale-105 hover:-skew-y-12 mb-3 '>{user.name}</h2><br />
+                    <p className='inline-block bg-indigo-600 text-white p-1 rounded-md hover:scale-105 hover:skew-y-12 mb-3'>Bio: {user.bio ? user.bio : 'Not Found'}</p><br />
+                    <h4 className='inline-block p-1 bg-blue-600 rounded-md text-white hover:scale-110 mb-2'>Location: {user.location ? user.location : 'Not Found'}</h4>
+                    <div className='flex justify-center gap-5'>
+                        <h3 className='bg-blue-600 p-1 rounded-md text-white'>Followers: {user.followers}</h3>
+                        <h3 className='bg-indigo-600 p-1 rounded-md text-white'>Following: {user.following}</h3>
+                    </div>
+                    <h5 className='mt-3 text-cyan-800'>Account Create: {user.created_at}</h5>
+                    
+                        <a className='inline-block p-2 text-blue-700 rounded hover:scale-110' href={user.html_url}>Go to github profile <AiOutlineArrowRight className='inline-block'></AiOutlineArrowRight> </a>
+                        
+                    
+                </div>
             </div>
-            <h5>Account create: {user.created_at}</h5>
-            <a className='p-2 text-blue-700 hover:underline rounded' href={user.html_url}>Click github profile</a>
         </div>
     );
 };
